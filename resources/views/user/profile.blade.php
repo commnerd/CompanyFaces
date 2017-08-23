cfc37cc32938dbfc6fd69251f75e83b4dc23f54f
Michael Miller <commnerd@gmail.com>
2017-08-22 22:32:14 -0600

    Update to styles and profile page

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,3 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+
+<header>
+    <h1>{{ $user->name }}</h1>
+</header>
 <aside>
     <img src="{{ Storage::url($user->photo->image_path) }}">
+    <ul>
+        <li><a href="mailto:{{ $user->email }}">{{ $user->email }}</a></li>
+    </ul>
 </aside>
+<section>
+    <ul>
+    @if($supervisor === NULL)
+        <li>This person is the {{ $user->position }}.</li>
+    @else
+        <li>{{ $supervisor->name }}</li>
+        @while(NULL !== ($supervisor = $supervisor->supervisor)):
+            <li>{{ $supervisor->name }}</li>
+        @endwhile
+    @endif
+
+    </ul>
+    <div>
+        <h1>Bio</h1>
+        {{$user->biography}}
+    </div>
+</section>
+<footer>
+
+</footer>
+@endsection
