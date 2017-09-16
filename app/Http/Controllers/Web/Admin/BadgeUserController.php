f7296a532417d28a1b31549cfa658ebadf0f7a51
Michael Miller <commnerd@gmail.com>
2017-09-16 09:42:04 -0600

    Remove all stand_along instances of badges before reassigning to user

--- app/Http/Controllers/Web/Admin/BadgeUserController.php
+++ app/Http/Controllers/Web/Admin/BadgeUserController.php
@@ -10,31 +10,56 @@ use App\User;
 
 class BadgeUserController extends AdminController
 {
+    /**
+     * Present screen to assign badges to people
+     * @param  User     $user The user to assign the badge
+     * @return Response       Redirect or view depending on scenario
+     */
     public function assign(User $user): Response {
         $badges = Badge::paginate(15);
+
+        // If no badges to assign, redirect to badge creation page
         if($badges->count() === 0) {
             session()->flash('warning', 'No badges available to assign, please create one.');
             return response(null, 302)->header('Location', route('admin.badges.create'));
         }
+
+        // Provide included badges for deletion on update
         $presentedBadges = [];
         $user->hasBadge($badges[0]);
         foreach($badges as $badge) {
             array_push($presentedBadges, $badge->id);
         }
         $presentedBadges = implode(',', $presentedBadges);
+
+        // Provide view
         return response()->view('admin.badges.assign', compact('user', 'badges', 'presentedBadges'));
     }
 
+    /**
+     * Save the state of the presented badges
+     * @param  User     $user    User whose badges are being updated
+     * @param  Request  $request The request holding the badges
+     * @return Response          Redirect for successful badge update
+     */
     public function save(User $user, Request $request): Response {
+        // Delete the presented badges for repopulation
         BadgeUser::where('user_id', $user->id)
             ->whereIn('badge_id', explode(',', $request->input('presentedBadges')))
             ->delete();
+
+        // Repopulate selected badges
         foreach(array_keys($request->input('badges') ?? []) as $badge) {
+            if($badge->stand_alone) {
+                BadgeUser::where('badge_id', $badge)->delete();
+            }
             BadgeUser::create([
                 'user_id' => $user->id,
                 'badge_id' => $badge,
             ]);
         }
+
+        // Provide view
         session()->flash('success', 'Successfully saved badges for '.$user->name.'.');
         return response(null, 302)->header('Location', route('users.show', ['user'=>$user]));
     }
